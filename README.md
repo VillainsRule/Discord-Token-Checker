2c615718848a75741239991ed4274c9929b9fbbf
thonk <101288516+VillainsRule@users.noreply.github.com>
2023-11-24 17:17:33 -0500

    add a final result file, change logging style

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ If you find a bug, open an [issue](https://github.com/VillainsRule/Discord-Token
 ## Installation
 1. Install [NodeJS](https://nodejs.org/en/download/current).
 2. Open your Terminal (Mac)/Powershell (Windows) app.
-3. Clone the repo from Git: `git clone https://github.com/VillainsRule/Discord-Token-Checker.git && cd cd Discord-Token-Checker`
+3. Clone the repo from Git: `git clone https://github.com/VillainsRule/Discord-Token-Checker.git && cd Discord-Token-Checker`
 4. Install dependencies: `npm i`
 5. Add the input tokens to tokens.txt.<br>On Mac, run `open -e tokens.txt`. On Windows, run `notepad tokens.txt`.<br><br>
 6. Then, run the program! `node .`
--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import fs from 'fs';
 import figlet from 'figlet';
 import gradient from 'gradient-string';
+import YAML from 'yaml';
 
 console.clear();
 console.log(gradient.fruit(
@@ -21,10 +22,10 @@ fs.mkdirSync('./output', { recursive: true, force: true });
 
 let read = 0;
 let checked = 0;
-let nitro = 0;
-let working = 0;
-let locked = 0;
-let invalid = 0;
+let nitro = [];
+let working = [];
+let locked = [];
+let invalid = [];
 
 let headers = {
     'Accept': '*/*',
@@ -38,44 +39,52 @@ let headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.41 Safari/537.36'
 };
 
-const check_token = (token) => {
-    axios.get(`https://discord.com/api/v9/users/@me/billing/subscriptions`, {
-        headers: {
-            Authorization: token,
-            ...headers
-        }
-    }).then((res) => {
-        if (res.status === 200 && res.data.length) {
-            console.log(gradient(['#f06fe9', '#eb65e3'])(`Nitro Token: `) + gradient.instagram(token));
-            nitro++;
+const end = () => {
+    console.log(`\n${gradient.instagram(`Checked ${checked} tokens! `)} ${gradient(['#f06fe9', '#eb65e3'])(`${nitro.length} (working) nitro,`)} ${gradient(['#0af030', '#0af030'])(`${working.length} working,`)} ${gradient(['#f0b30a', '#f0b30a'])(`${locked.length} locked,`)} ${gradient(['red', 'red'])(`& ${invalid.length} invalid.`)}`);
+    fs.writeFileSync('./output/final.yaml', YAML.stringify({ accounts: { nitro, working, locked, invalid } }));
+    process.exit(0);
+};
+
+const check_token = async (token) => {
+    try {
+        let res = await axios.get(`https://discord.com/api/v9/users/@me`, {
+            headers: {
+                Authorization: token,
+                ...headers
+            }
+        });
+
+        if (res.status === 200 && res.data.premium_type !== 0) {
+            console.log(`${gradient(['#f06fe9', '#eb65e3'])(`[ Nitro ] `)}\n     ${(res.data.discriminator !== '0' ? gradient.vice(res.data.username + '#' + res.data.discriminator) : gradient.vice('@' + res.data.username))}\n     ${gradient.vice(token)}`);
+            nitro.push({ name: res.data.username + '#' + res.data.discriminator, token });
             fs.appendFile('./output/nitro.txt', `${token}\n`, (err) => (err) ? console.log(tinycolor('magenta')(`Cannot save token to nitro file. ${err}`)) : false);
         } else if (res.status === 200) {
-            console.log(gradient(['#0af030', '#0af030'])(`Working Token: `) + gradient.instagram(token));
-            working++;
+            console.log(`${gradient(['#0af030', '#0af030'])(`[ Working ] `)}\n     ${(res.data.discriminator !== '0' ? gradient.vice(res.data.username + '#' + res.data.discriminator) : gradient.vice('@' + res.data.username))}\n     ${gradient.vice(token)}`);
+            working.push({ name: res.data.username + '#' + res.data.discriminator, token });
             fs.appendFile('./output/working.txt', `${token}\n`, (err) => (err) ? console.log(tinycolor('magenta')(`Cannot save token to working file. ${err}`)) : false);
         } else console.log(gradient(['magenta', 'magenta'])(`Unknown Error with code ${res.statusCode}. `) + gradient.instagram(token));
 
         checked++;
-        if (checked === tokenFile.split('\n').length) console.log(`\n${gradient.instagram(`Checked ${checked} tokens! `)} ${gradient(['#f06fe9', '#eb65e3'])(`${nitro} (working) nitro,`)} ${gradient(['#0af030', '#0af030'])(`${working} working,`)} ${gradient(['#f0b30a', '#f0b30a'])(`${locked} locked,`)} ${gradient(['red', 'red'])(`& ${invalid} invalid.`)}`);
-    }).catch((err) => {
+        if (checked === tokenFile.split('\n').length) end();
+    } catch (err) {
         if (!err.response) {
-            console.log(`\n${gradient.morning(`Unknown Error: `)}"${err}"${gradient.morning(`. Please report this in our Discord server. Quitting program...`)}\n`);
+            console.log(`\n${gradient.morning(`Unknown Error: `)}"${err}"${gradient.morning(`. Please open an issue & report this error on Github. Quitting program...`)}\n`);
             return process.exit(0);
         };
 
         if (err.response.status === 401) {
-            console.log(gradient(['red', 'red'])(`Token Invalid :|  > `) + gradient.instagram(token));
+            console.log(`${gradient(['red', 'red'])(`[ Invalid ] `)}\n     ${gradient.vice(token)}`);
             fs.appendFile('./output/invalid.txt', `${token}\n`, (err) => (err) ? console.log(tinycolor('magenta')(`Cannot save token to invalid file. ${err}`)) : false);
-            invalid++;
+            invalid.push({ token });
         } else if (err.response.status === 403) {
-            console.log(gradient(['#f0b30a', '#f0b30a'])(`Token Locked :(   > `) + gradient.instagram(token));
+            console.log(`${gradient(['#f0b30a', '#f0b30a'])(`[ Locked ] `)}\n     ${gradient.vice(token)}`);
             fs.appendFile('./output/locked.txt', `${token}\n`, (err) => (err) ? console.log(tinycolor('magenta')(`Cannot save token to locked file. ${err}`)) : false);
-            locked++;
+            locked.push({ token });
         } else console.log(gradient(['magenta', 'magenta'])(`Unknown Error with code ${err.response.status}. `) + gradient.instagram(token));
 
         checked++;
-        if (checked === tokenFile.split('\n').length) console.log(`\n${gradient.instagram(`Checked ${checked} tokens! `)} ${gradient(['#f06fe9', '#eb65e3'])(`${nitro} (working) nitro,`)} ${gradient(['#0af030', '#0af030'])(`${working} working,`)} ${gradient(['#f0b30a', '#f0b30a'])(`${locked} locked,`)} ${gradient(['red', 'red'])(`& ${invalid} invalid.`)}`);
-    });
+        if (checked === tokenFile.split('\n').length) end();
+    };
 };
 
 tokenFile.split('\n').forEach(token => {
